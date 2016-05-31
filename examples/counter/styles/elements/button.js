7e5237a7bb3fe34fa6474a303f9986cf69bc1193
Kildyt <kildyt@gmail.com>
2016-06-01 00:03:26 +0200

    Counter example added

---
+++ examples/counter/styles/elements/button.js
@@ -0,0 +1,11 @@
+Flow {
+    document.query: 'button'
+    background.color: 'gray'
+    padding: '10 20'
+    margin: 5
+
+    for ('#text') {
+        color: 'white'
+        font.pixelSize: 20
+    }
+}
