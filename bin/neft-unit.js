1795261794b29a556a27d32492d0c07477b12115
Kildyt <kildyt@gmail.com>
2016-06-06 22:43:02 +0200

    'neft-unit' now register babel

--- bin/neft-unit.js
+++ bin/neft-unit.js
@@ -3,6 +3,10 @@
 process.env.NODE_PATH = './:'+__dirname+'/..:'+__dirname+'/../node_modules';
 require('module').Module._initPaths();
 require('coffee-script/register');
+require('babel-register')({
+    presets: ['es2015'],
+    ignore: /(bundle|build)\/(neft|app)-[a-z]+-[a-z]+\.js|node_modules/
+});
 const fs = require('fs');
 global.Neft = require('../cli/bundle/neft-node-develop.js');
 global.app = require(fs.realpathSync('./build/app-node-develop.js'));
