e36ba790cb60b4341825823c41b0f0f76771c5ba
Kildyt <kildyt@gmail.com>
2016-07-02 11:21:54 +0200

    Android runtime x86 abi added; V8 5.0

--- runtimes/android/app/src/main/jni/Application.mk
+++ runtimes/android/app/src/main/jni/Application.mk
@@ -1,7 +1,7 @@
-APP_ABI := armeabi-v7a
+APP_ABI := armeabi armeabi-v7a x86
 
-APP_STL := stlport_static
-APP_OPTIM := debug
+APP_STL := c++_static
+APP_OPTIM := release
 APP_PLATFORM := android-17
 NDK_TOOLCHAIN_VERSION := 4.8
 
