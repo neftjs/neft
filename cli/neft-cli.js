8e717afd55ad52eb97a1e517bb7f81aba1716999
Kildyt <kildyt@gmail.com>
2016-07-16 10:48:59 +0200

    Babel now omits neft bundle file

--- cli/neft-cli.js
+++ cli/neft-cli.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const moduleCache = require('lib/moduleCache');
-moduleCache.register();
+moduleCache.registerFilenameResolver();
+moduleCache.registerCoffeeScript();
 require('./init.coffee');
 
 module.exports = Neft
