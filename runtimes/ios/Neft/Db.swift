75aeb53974e8039928b732d26ec36abc613c79fb
Kildyt <kildyt@gmail.com>
2017-02-20 23:23:11 +0100

    Thread safe DB on ios runtime

--- runtimes/ios/Neft/Db.swift
+++ runtimes/ios/Neft/Db.swift
@@ -39,17 +39,20 @@ class Db {
         }
     }
     private static func resolve(_ path: String, _ id: CGFloat, _ completion: @escaping (URL) throws -> String?) {
-        thread {
+        var responseArgs: [Any?]? = nil
+        thread({
             let dir = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-            let client = App.getApp().client
             var result: String? = nil
             do {
                 result = try completion(dir!.appendingPathComponent("__neftDb_" + path))
             } catch {
-                client?.pushEvent(RESPONSE, args: [id, error.localizedDescription, nil])
+                responseArgs = [id, error.localizedDescription, nil]
                 return
             }
-            client?.pushEvent(RESPONSE, args: [id, nil, result])
+            responseArgs = [id, nil, result]
+        }) {
+            let client = App.getApp().client
+            client?.pushEvent(RESPONSE, args: responseArgs)
         }
     }
 }
