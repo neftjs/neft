5bc02ddce21b7689ba48602c7694a5a02cee4ebe
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-23 18:40:50 +0000

    Deploy neft.io to github.com/Neft-io/neft.git:gh-pages

--- scripts/svg-render.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    var codes = document.querySelectorAll('code.svg');
-
-    for (var i = 0; i < codes.length; i++) {
-        var elem = codes[i];
-        elem.innerHTML = elem.textContent;
-    }
-}());
