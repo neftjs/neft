64b3b6f6b40b1e604d9f161c1b34a7cf619e02b0
Kildyt <kildyt@gmail.com>
2018-10-16 20:44:00 +0200

    Fix for handling errors when calling Neft.render

--- packages/neft-core/src/initializer.js
+++ packages/neft-core/src/initializer.js
@@ -1,6 +1,7 @@
 const Renderer = require('./renderer')
 const Document = require('./document')
 const Element = require('./document/element')
+const eventLoop = require('./event-loop')
 
 const windowElement = new Element.Tag()
 windowElement.props.set('n-style', ['__default__', 'item'])
@@ -23,11 +24,13 @@ Renderer.setWindowItem(windowElement.style)
 
 let renderer = null
 exports.init = (DocumentFile, options) => {
-  if (renderer) {
-    renderer.element.parent = null
-    renderer.revert()
-  }
-  renderer = DocumentFile()
-  renderer.render(options)
-  renderer.element.parent = windowElement
+  eventLoop.setImmediate(() => {
+    if (renderer) {
+      renderer.element.parent = null
+      renderer.revert()
+    }
+    renderer = DocumentFile()
+    renderer.render(options)
+    renderer.element.parent = windowElement
+  })
 }
