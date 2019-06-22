2e36d48b879b888a3f1042eca360b09257173c86
Kildyt <kildyt@gmail.com>
2019-06-22 10:35:14 +0200

    Removed unused native libraries

--- packages/lib/neft-ambient-sound/native/android/AmbientSoundExtension.java
+++
@@ -1,9 +0,0 @@
-package io.neft.extensions.ambientsound_extension;
-
-import io.neft.renderer.NativeItem;
-
-public final class AmbientSoundExtension {
-    public static void register() {
-        NativeItem.registerItem(AmbientSoundItem.class);
-    }
-}
