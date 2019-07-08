40cddd38646414b7165f13317102c644a59ce6e2
Kildyt <kildyt@gmail.com>
2019-07-08 18:46:12 +0200

    Fixed opening ios projects with pods

--- packages/cli/neft-cli/src/runner/ios/index.js
+++ packages/cli/neft-cli/src/runner/ios/index.js
@@ -1,6 +1,16 @@
+const path = require('path')
 const cp = require('child_process')
+const fs = require('fs').promises
 const { promisify } = require('util')
 
 module.exports = async () => {
-  await promisify(cp.exec)('open dist/ios/Neft.xcodeproj')
+  const dir = 'dist/ios'
+  const xcworkspace = path.join(dir, 'Neft.xcworkspace')
+  const xcodeproj = path.join(dir, 'Neft.xcodeproj')
+  try {
+    await fs.stat(xcworkspace)
+    await promisify(cp.exec)(`open ${xcworkspace}`)
+  } catch (error) {
+    await promisify(cp.exec)(`open ${xcodeproj}`)
+  }
 }
