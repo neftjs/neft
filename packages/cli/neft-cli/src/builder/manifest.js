ed08fdaf57da443d7cdefab5334f6cc110eab51f
Kildyt <kildyt@gmail.com>
2019-07-30 17:49:33 +0200

    Always provide manifest file with default values

--- packages/cli/neft-cli/src/builder/manifest.js
+++ packages/cli/neft-cli/src/builder/manifest.js
@@ -14,16 +14,23 @@ exports.produceManifest = async ({ target, defaultManifest }) => {
     // already exists
   }
 
-  const filepath = path.join(realpath, `/manifest/${target}.yaml`)
+  let filepath = path.join(realpath, `/manifest/${target}.yaml`)
+  try {
+    await fs.stat(filepath)
+  } catch (error) {
+    filepath = path.join(realpath, `/manifest/${target}.yml`)
+  }
+
   let manifest
   try {
     manifest = yaml.safeLoad(await fs.readFile(filepath, 'utf-8'))
     manifest = util.mergeDeepAll({}, defaultManifest, manifest)
   } catch (_error) {
     // file doesn't exist
-    await fs.writeFile(filepath, yaml.safeDump(defaultManifest))
     manifest = defaultManifest
   }
+
+  await fs.writeFile(filepath, yaml.safeDump(manifest))
 
   return manifest
 }
